5494d720546464efad2c7877bf2b63ffcd656ad3
hugomufraggi <hugo.mufraggi@epitech.eu>
2025-04-21 22:14:50 +0200

    add

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import AppSidebar from '@/components/AppSidebar.vue'
+</script>
+
+<template>
+  <head>
+    <link href="/src/index.css" rel="stylesheet" />
+    <title></title>
+  </head>
+
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <RouterView />
+    </main>
+  </SidebarProvider>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="green">Coucou</h1>
+</template>
