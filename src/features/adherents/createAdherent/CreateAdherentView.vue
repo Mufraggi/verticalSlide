9e313184302e8ca72ae38f675366923a8c7c0587
hugomufraggi <hugo.mufraggi@epitech.eu>
2025-04-23 19:10:12 +0200

    add first implementation

--- src/features/adherents/createAdherent/CreateAdherentView.vue
+++ src/features/adherents/createAdherent/CreateAdherentView.vue
@@ -2,8 +2,8 @@
   <div>
     <AdherentForm @submit="onSubmitForm" />
 
-    <div v-if="store.success">✔️ Créé !</div>
-    <div v-if="store.error">❌ {{ store.error }}</div>
+    <div v-if="status === 'success'">✔️ Créé !</div>
+    <div v-if="status ==='error'">❌ {{ store.error }}</div>
   </div>
 </template>
 
@@ -15,18 +15,38 @@ import {
 } from '@/features/adherents/createAdherent/createAdherent.usecase.ts'
 import AdherentForm from '@/features/adherents/createAdherent/components/AdherentForm.vue'
 import { useRouter } from 'vue-router'
-const {execute} = useCreateAdherentUseCase()
+import { useAdherentEvents } from '@/features/adherents/createAdherent/adherentCreated.event.ts'
+import { ref } from 'vue'
+
+const { onCreated, onCreationStarted, onCreationFailed } = useAdherentEvents()
+const { execute } = useCreateAdherentUseCase()
 const router = useRouter()
 const store = useCreateAdherentStore()
+
+
+const status = ref<'success'| 'error'| 'pending' | null>(null)
+const error = ref<string | null>(null)
+onCreated((adherent) => {
+  console.log('Adherent created:', adherent)
+  status.value = 'success'
+})
+onCreationStarted((adherent) => {
+  console.log('Adherent created:', adherent)
+  status.value = 'pending'
+})
+onCreationFailed((adherent) => {
+  console.log('Adherent created:', adherent)
+  status.value = 'error'
+  error.value = adherent
+})
 
 const onSubmitForm: (formData: CreateAdherentCommand) => Promise<void> = async (
   formData: CreateAdherentCommand,
 ) => {
   try {
     await execute(formData)
-    await router.push('/about')
+   // await router.push('/about')
   } catch (error) {
-    // L'erreur est déjà gérée dans le store
     console.error("Erreur lors de la création de l'adhérent:", error)
   }
 }
