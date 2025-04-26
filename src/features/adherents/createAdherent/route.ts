d4f5cd3e80c2ae39b3c6a035c8259e4bda5ac129
hugomufraggi <hugo.mufraggi@epitech.eu>
2025-04-26 21:05:01 +0200

    add adherent data table

--- src/features/adherents/createAdherent/route.ts
+++ src/features/adherents/createAdherent/route.ts
@@ -1,7 +1,7 @@
 import CreateAdherentView from '@/features/adherents/createAdherent/ui/CreateAdherentView.vue'
 
 export const createAdherentRoute = {
-  path: '/',
-  name: 'home',
+  path: '/adherents/create',
+  name: 'adherents.create',
   component: CreateAdherentView,
 }
---
+++ src/features/adherents/listAdherent/ui/ListAdherents.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { columns } from './components/columns'
+import DataTable from '@/features/adherents/listAdherent/ui/components/data-table.vue'
+import { useListAdherentUseCase } from '@/features/adherents/listAdherent/listAdherent.usecase' // Importez votre use case
+import { useAdherentListEvents } from '@/features/adherents/listAdherent/adherentList.event'
+import { useAdherentsStore } from '@/features/adherents/listAdherent/listAdherent.store.ts'
+import { Skeleton } from '@/components/ui/skeleton'
+
+// Utilisez le store Pinia pour récupérer les données et l'état
+const adherentsStore = useAdherentsStore()
+const { execute } = useListAdherentUseCase()
+const events = useAdherentListEvents()
+
+// Configurez les gestionnaires d'événements pour mettre à jour le store
+events.onListStarted(() => {
+  adherentsStore.startLoading()
+})
+
+events.onRetrieved((adherents) => {
+  adherentsStore.setAdherents(adherents)
+})
+
+events.onListFailed((error) => {
+  adherentsStore.setError(error)
+})
+
+// Chargez les données lors du montage du composant
+onMounted(async () => {
+  await execute()
+})
+</script>
+
+<template>
+  <div class="container py-10 mx-auto">
+    <div v-if="adherentsStore.isLoading" class="flex flex-col space-y-3">
+      <Skeleton class="h-[125px] w-[250px] rounded-xl" />
+      <div class="space-y-2">
+        <Skeleton class="h-4 w-[250px]" />
+        <Skeleton class="h-4 w-[200px]" />
+      </div>
+    </div>
+
+    <div v-if="adherentsStore.isLoading">Chargement en cours...</div>
+    <div v-else-if="adherentsStore.hasError" class="error-message">
+      Erreur: {{ adherentsStore.error }}
+    </div>
+    <DataTable v-else :columns="columns" :data="adherentsStore.adherents" />
+  </div>
+</template>
+
+<style scoped>
+.error-message {
+  color: red;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
